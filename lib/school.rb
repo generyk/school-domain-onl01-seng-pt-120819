f6a891b8159f87acd5932ae8beaa8e719624a3c0
Eryk Rachwal <erykrachwal@yahoo.com>
2020-01-09 04:35:59 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,9 +5,20 @@ class School
     @name = name 
     @roster = {}
   end 
-
-
-  def roster=(roster)
-    @roster = 
+  
+  def add_student(student, level)
+    roster[level] ||= []
+    roster[level] << student 
   end 
+  
+  def grade(level)
+    roster.detect do |x, y|
+      if x == level 
+        return y 
+      end 
+    end 
+  end 
+  
+  def sort 
+    nu
 end 
